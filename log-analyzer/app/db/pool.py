f65b1a989e8cfdbbe09f8d6199e1d5e415cc5146
Maaz <maaztahir2004.32@gmail.com>
2026-05-04 22:11:14 +0500

    Added a global db connection inside pool.py which can be used in routes as dependency

--- log-analyzer/app/db/pool.py
+++ log-analyzer/app/db/pool.py
@@ -1,6 +1,8 @@
 #Add a global db connection here
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from app.core.config import config
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 engine = create_async_engine(
@@ -14,3 +16,17 @@ AsyncSessionLocal = async_sessionmaker(
     bind=engine,
     expire_on_commit=False
 )
+
+
+Base = declarative_base()
+
+# Dependency for FastAPI
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
